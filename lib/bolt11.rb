519688efaf56d843e11ac6c856bfe78d323155c2
johnta0 <bitjohnta@gmail.com>
2018-02-04 21:07:10 +0900

    Generate files and directories for gem

---
+++ lib/bolt11.rb
@@ -0,0 +1,5 @@
+require "bolt11/version"
+
+module Bolt11
+  # Your code goes here...
+end
---
+++ spec/bolt11_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Bolt11 do
+  it "has a version number" do
+    expect(Bolt11::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
